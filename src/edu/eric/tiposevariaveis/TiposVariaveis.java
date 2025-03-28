faa79e741bb17e427dff3deb338704c731c7d1a4
Eric Tensol Vieira <62815764+erictensoldesenvolve@users.noreply.github.com>
2025-03-28 10:24:41 -0300

    aprendendo sobre tipos e variaveis

---
+++ src/edu/eric/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,11 @@
+package edu.eric.tiposevariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salario = 233.99;
+        
+        //a palavra final significa que não pode mudar o valor, vira uma CONSTANTE, sempre de caixa alta.
+        final double VALOR_DE_PI =3.14;
+        
+    }
+}
